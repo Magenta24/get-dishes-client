7045e486a331cf38249712cb46c5ed5d22606c56
Jagoda Tata <sc20jmt@leeds.ac.uk>
2022-11-18 14:40:49 +0000

    adding dynamic select field with data got from service1

--- app/routes.py
+++ app/routes.py
@@ -3,14 +3,40 @@ from app.forms import InputForm
 from flask import render_template, request
 from config import GRAPH_SERVICE_URL
 import requests
+import json
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():    
-    form = InputForm(['xd', 'xxd', 'xddd', 'xdxdxdx'])
+    json_categories = requests.get('http://127.0.0.1:5000/ingredient_categories')
+    categories_names = []
+    ingredients = []
+    
+    for c in json_categories.json():
+        categories_names.append(json_categories.json()[c]['name'])
+
+    json_ingredients = requests.get('http://127.0.0.1:5000/ingredients/' + json_categories.json()['1']['name'])
+
+    for i in json_ingredients.json()['ingredients']:
+        ingredients.append(i['name'])
+        
+    form = InputForm(categories_names, ingredients)
+
+    if request.method == 'POST':
+        return form.category1.data
     return render_template("index.html", form=form) 
 
-@app.route('/algorithm', methods=['POST'])
+# returns json with ingredients with given category
+@app.route('/ingredients/<category>')
+def ingredients(category):
+    pass
+
+# returns all dishes matching the user's selected ingredients  
+@app.route('/check_dishes', methods=[])
+def check_dishes():
+    pass
+
+@app.route('/recipie', methods=['POST'])
 def algorithm():    
     algorithm = request.args.get("algorithm", default="")
     nodes = request.args.get("nodes", default=[])
