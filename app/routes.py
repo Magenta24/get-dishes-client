c1281d9a13f1c9344b105d4cded2b7cbfb70bc74
Dominik Strak <dominikstrak2001@gmail.com>
2022-11-21 12:50:21 +0000

    full backend

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,18 @@
 from app import app
 from app.forms import InputForm
-from flask import render_template, request
-from config import SERVICE1_URL, SERVICE_2_URL
+from flask import render_template, request, send_from_directory
+from config import API_KEY, DEFAULT_IMAGE_PATH, IBM_URL, SERVICE1_URL, SERVICE_2_URL, TTS_FOLDER
 import requests
+from ibm_watson import TextToSpeechV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+
+authenticator = IAMAuthenticator(API_KEY)
+text_to_speech = TextToSpeechV1(
+    authenticator=authenticator
+)
+
+text_to_speech.set_service_url(IBM_URL)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -38,22 +48,42 @@ def check_dishes():
 
     print(user_ingredients)
 
-    dishes_response = requests.post(SERVICE1_URL + '/dishes', json=user_ingredients)
+    dishes_response = requests.get(SERVICE1_URL + '/all_dishes')
 
     dishes_and_recipes = {}
 
-    for dish in dishes_response:
-        print(dish, type(dish))
-        print(SERVICE_2_URL + f'/dish/{str(dish)}')
-        post_recipes = requests.get(SERVICE_2_URL + f'/dish/{str(dish)}')
-        print(post_recipes)
+    print(dishes_and_recipes)
+    print(dishes_response.json())
+
+    print("I HARDCODED THE EXAMPLES PLEASE REMOVE MEEEEEEEEEEEEEEEEEEEEEE")
+    HARDCODED_EXAMPLE = ['spaghetti']
+
+    for dish in HARDCODED_EXAMPLE:
+    # for dish in dishes_response.json():
+        post_recipes = requests.get(SERVICE_2_URL + f'/dishes/{str(dish)}')
+
         recipes = post_recipes.json()
         if len(recipes) > 0:
+            if str(dish) not in dishes_and_recipes.keys():
+                dishes_and_recipes[str(dish)] = dict()
+
             dishes_and_recipes[str(dish)]['recipe'] = recipes[0]['recipe']
             if 'imageName' in recipes[0].keys():
                 dishes_and_recipes[str(dish)]['image_url'] = SERVICE_2_URL + "/downloadFile/" + recipes[0].get('imageName')
             else:
-                dishes_and_recipes[str(dish)]['image_url'] = "DEFAULT_IMAGE_PATH"
+                dishes_and_recipes[str(dish)]['image_url'] = DEFAULT_IMAGE_PATH
 
+            # with open(f'{TTS_FOLDER}{dish}.mp3', 'wb') as audio_file:
+            #     audio_file.write(
+            #         text_to_speech.synthesize(
+            #             recipes[0]['recipe'],
+            #             voice='en-US_AllisonV3Voice',
+            #             accept='audio/mp3'        
+            #         ).get_result().content)
     print(dishes_and_recipes)
-    return dishes_and_recipes
+    return render_template("dishes.html", dishes = dishes_and_recipes)
+
+
+@app.route('/get_recipe_audio/<string:dish_name>')
+def get_recipe_audio(dish_name):
+    return send_from_directory(TTS_FOLDER, f'{dish_name}.mp3', as_attachment=True)
--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 # Enabling CSRF protection
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'secret-key-1234-#$%^'
@@ -8,5 +9,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SERVICE1_URL = 'http://127.0.0.1:5000'
 
+SERVICE_2_URL = 'http://localhost:8080/api'
 
-SERVICE_2_URL = 'http://localhost:8080'
+API_KEY = '4nohXYsTp0V8_-JhC5JHP0-Eg0VSU6iCraaWkQK3yqXD'
+IBM_URL = 'https://api.eu-gb.text-to-speech.watson.cloud.ibm.com/instances/50f3a644-4baf-4860-a6c9-8f97f35d3016'
+
+TTS_FOLDER = Path(basedir, "tts")
+
+DEFAULT_IMAGE_PATH = "images/no_image_burger.png"
